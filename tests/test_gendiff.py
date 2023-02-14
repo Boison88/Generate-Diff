29eeb8500b985ec901c2f2d6bd3cd5ecabba5c03
boison88 <boison88@mail.ru>
2023-02-14 22:30:57 +0300

    step 4.5 fix tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,18 +1,10 @@
-from diff import generate_diff
+from gendiff.diff import generate_diff
 
 
 def test_gendiff():
-    file1_path = '/home/boison/python-project-50/tests/fixtures/file1.json'
-    file2_path = '/home/boison/python-project-50/tests/fixtures/file2.json'
+    file1_path = '.tests/fixtures/file1.json'
+    file2_path = '.tests/fixtures/file2.json'
     result = generate_diff(file1_path, file2_path)
-    output = '\n'.join([
-        '{',
-        '- follow: false',
-        '  host: hexlet.io',
-        '- proxy: 123.234.53.22',
-        '- timeout: 50',
-        '+ timeout: 20',
-        '+ verbose: true',
-        '}'
-    ])
-    assert result == output
+    output = open('.tests/fixtures/output_json.txt')
+
+    assert result == output.read()
