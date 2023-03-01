c03d42e79368aa3e393bc918da8ac021b2797af1
boison88 <boison88@mail.ru>
2023-03-01 22:53:13 +0300

    step 8.8 just rename

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,8 +6,8 @@ from tests import get_full_path
 @pytest.mark.parametrize('input1, input2, expected', [
     ('file1.json', 'file2.json', 'result.txt'),
     ('file1.yaml', 'file2.yaml', 'result.txt'),
-    ('file1_tree.json', 'file2_tree.json', 'result_nested.txt'),
-    ('file1_tree.yaml', 'file2_tree.yaml', 'result_nested.txt')
+    ('file1_tree.json', 'file2_tree.json', 'result_tree.txt'),
+    ('file1_tree.yaml', 'file2_tree.yaml', 'result_tree.txt')
 ])
 def test_generate_diff(input1, input2, expected):
     file1, file2 = map(get_full_path, (input1, input2))
@@ -17,7 +17,7 @@ def test_generate_diff(input1, input2, expected):
 
 
 @pytest.mark.parametrize('input1, input2', [
-    ('result.txt', 'result_nested.txt'),
+    ('result.txt', 'result_tree.txt'),
 ])
 def test_exception_in_generate_diff(input1, input2):
     file1, file2 = map(get_full_path, (input1, input2))
